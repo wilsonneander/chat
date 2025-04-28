b05a7664010eb92c9fe740f5cf71e891af0bae19
Wilson Neander <https://github.com/wilsonneander/portf-lio.git>
2025-04-28 15:55:24 -0300

    adicionar react com vite

---
+++ frontend/src/components/ChatCard/chat-card.tsx
@@ -0,0 +1,18 @@
+import './chat-card.css';
+
+export  default function ChatCard() {
+    return (
+        <section className="chat">
+            <section className="chat__messages">
+                
+            </section>
+
+            <form className="chat__form">
+                <input type="text" className="chat__input" placeholder="Digite uma mensagem" required />
+                <button type="submit" className="chat__button">
+                    <span className="material-symbols-outlined">send</span>
+                </button>
+            </form>
+        </section>
+    )
+}
---
+++ frontend/src/components/Login/login.tsx
@@ -0,0 +1,11 @@
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector(".login__input")
+
+    <section class="login" >
+            <h2>Login</h2>
+            <form class="login__form">
+                <input type="text" class="login__input" placeholder="Seu nome" required />
+                <button type="submit" class="login__button">Entrar</button>
+            </form>
+    </section >
